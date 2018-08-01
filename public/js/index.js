0248cdefc7496ea66f5da46039bdb2aef57b1488
Felix Avelar <felixavco@gmail.com>
2018-08-01 01:45:18 -0600

    added navbar and footer

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.slider').slider();
+
+  /* menu effect */
+    let flag = true;
+    const menu = $('#menu');
+
+    $('section, li').on('click', () => {
+        menu.animate({left: '-100%'}, 1000);
+        flag = true;
+    })
+
+    $('.menu-btn').on('click', () => {
+
+      if(flag){
+        menu.animate({left: '0'}, 500);
+        flag = false;
+      } else {
+        menu.animate({left: '-100%'}, 1000);
+        flag = true;
+      }
+
+    })
+  /* menu effect */
+  
+  document.querySelector('#year').innerHTML = new Date().getFullYear();
+
 });
     
