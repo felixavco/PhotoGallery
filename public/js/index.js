b6a96872f4e7b5742de1b68d5a93e77b96ce6c6b
Felix Avelar <felixavco@gmail.com>
2018-08-04 13:52:40 -0600

    finished auto scroll and animations

--- public/js/index.js
+++ public/js/index.js
@@ -35,6 +35,29 @@ $(document).ready(() => {
   
   document.querySelector('#year').innerHTML = new Date().getFullYear();
 
+  /* Implement Waypoints */
+  const gal1 = $('.gal1');
+  const gal2 = $('.gal2');
+  const gal3 = $('.gal3');
+  const form = $('.form');
+  const about = $('.about-wrapper');
+  const services = $('.service');
+
+  const wpoint = (element, animation, offset = '30') => {
+    element.waypoint(() => {
+      element.addClass(animation).css({opacity: '1'})
+    }, {offset: offset+'%'})
+  }
+
+  wpoint(gal1, 'fadeInUp', '75');
+  wpoint(gal2, 'fadeInUp', '50');
+  wpoint(gal3, 'fadeInUp', '75');
+  wpoint(about, 'fadeInUp', '95');
+  wpoint(services, 'fadeInUp', '95');
+  wpoint(form, 'fadeInUp', '95');
+
+ 
+
 
 
 });
